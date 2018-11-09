06c16db83599fb6826d3b5417da1ab74d4359b60
ricardocastilloisc <ricardocastilloisc@gmail.com>
2018-11-09 15:18:49 -0500

    4.9. RouterLink y RouterLinkActive - Completando las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { HomeComponent } from './components/home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'heroes', component: HeroesComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'routePath' }
 ];
 
